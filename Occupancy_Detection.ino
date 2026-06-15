05948fd6b8b1b7372c7545321f7febe88079c5f6
Manaswi Dulla <dullamanaswi@gmail.com>
2026-06-15 17:33:30 +0530

    Update Occupancy_Detection.ino

--- Occupancy_Detection.ino
+++ Occupancy_Detection.ino
@@ -1,23 +1,30 @@
+// Real-Time Occupancy Detection System for Energy Efficiency
+
 #define PIR_PIN 2
 #define LED_PIN 13
 
 unsigned long lastMotionTime = 0;
-const unsigned long TIMEOUT = 10000;
+const unsigned long TIMEOUT = 10000; // 10 seconds
 
 void setup() {
+  // Configure input and output pins
   pinMode(PIR_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
 
+  // Initially turn OFF LED
   digitalWrite(LED_PIN, LOW);
 
+  // Start Serial Monitor
   Serial.begin(9600);
-  Serial.println("Real-Time Occupancy Detection System Started");
+  Serial.println("Occupancy Detection System Started");
 }
 
 void loop() {
 
+  // Read PIR sensor status
   bool motionDetected = digitalRead(PIR_PIN);
 
+  // If motion is detected
   if (motionDetected) {
     digitalWrite(LED_PIN, HIGH);
     lastMotionTime = millis();
@@ -25,11 +32,13 @@ void loop() {
     Serial.println("Occupancy Detected - Light ON");
   }
 
+  // Turn OFF LED after 10 seconds of inactivity
   if ((millis() - lastMotionTime) > TIMEOUT) {
     digitalWrite(LED_PIN, LOW);
 
     Serial.println("No Occupancy - Light OFF");
   }
 
+  // Small delay for stability
   delay(100);
 }
